839c6ab3a462f417131596488d68f483456a5b3b
sourav <mdsehab1@gmail.com>
2024-02-05 00:20:35 +0600

    updated category type interface

--- src/app/pages/categories/category-item/category-item.component.ts
+++ src/app/pages/categories/category-item/category-item.component.ts
@@ -1,20 +1,34 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
-import { Category } from '../category-type';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+  inject,
+} from '@angular/core';
+import { CategoryItem } from '../category-type';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { ConfirmationComponent } from '../../../common/confirmation/confirmation.component';
+import { CategoryService } from '../categor.service';
+import { Subject, takeUntil } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-category-item',
   standalone: true,
   imports: [],
-  providers: [BsModalService],
+  providers: [ToastrService],
   templateUrl: './category-item.component.html',
   styleUrl: './category-item.component.scss',
 })
-export class CategoryItemComponent {
+export class CategoryItemComponent implements OnDestroy {
   modalService = inject(BsModalService);
-  @Input() category: Category = {} as Category;
-  @Output() editCategoryEmit: EventEmitter<number> = new EventEmitter();
+  toaster = inject(ToastrService);
+  categoryService = inject(CategoryService);
+  ngUnsubscribe$: Subject<void> = new Subject();
+  @Input() category: CategoryItem = {} as CategoryItem;
+  @Output() editCategoryEmit: EventEmitter<{ id: number; parentId: number }> =
+    new EventEmitter();
 
   openDeleteConfirmModal() {
     const initialState = {
@@ -31,11 +45,24 @@ export class CategoryItemComponent {
     modalRef.content?.confirmed.subscribe((isConfirmed) => {
       if (isConfirmed) {
         modalRef.hide();
-        // make delete request
+        this.categoryService
+          .deleteCategory(this.category.id)
+          .pipe(takeUntil(this.ngUnsubscribe$))
+          .subscribe((res) => {
+            if (res) {
+              this.toaster.success('Deleted successfully');
+            } else {
+              this.toaster.error('Delete Failed!');
+            }
+          });
       } else {
-        // just hide the modal
         modalRef.hide();
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
   }
 }
